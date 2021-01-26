1d3edb6b83ac3d1e46af4a9251ab5fb253c204cc
Fayrushin-Fadis <69743960+Fayrushin-Fadis@users.noreply.github.com>
2021-01-26 08:56:15 +0500

    Added JavaScript

---
+++ js/theme.js
@@ -0,0 +1,12 @@
+'Use strict'
+
+
+function theme()
+{
+	let timeBody = document.getElementById('themeBody')
+	timeBody.style.backgroundColor = '#333333'
+	let timeNav = document.getElementById('themeNav')
+	timeNav.style.backgroundColor = '#666666'
+	let timeHeader = document.getElementById('themeHeader')
+	timeHeader.style.backgroundColor = '#666666'
+}
