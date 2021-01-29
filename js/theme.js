4c2a88ee47ab766c44b68f9077afbf2365f9dc91
Fayrushin-Fadis <69743960+Fayrushin-Fadis@users.noreply.github.com>
2021-01-29 18:23:52 +0500

    My site Version 2.0

--- js/theme.js
+++ js/theme.js
@@ -1,26 +1,63 @@
 'Use strict'
 
-
-function blackTheme()
+function themeBlack()
 {
-	let timeBody = document.getElementById('themeBody')
-	timeBody.style.backgroundColor = '#333333'
-	let timeNav = document.getElementById('themeNav')
-	timeNav.style.backgroundColor = '#666666'
-	let timeHeader = document.getElementById('themeHeader')
-	timeHeader.style.backgroundColor = '#666666'
-	let timeFooter = document.getElementById('themeFooter')
-	timeFooter.style.backgroundColor = '#333333'
+	let timeVariable = "";
+	
+	// Fayrushin Fadis
+	timeVariable = document.getElementById('FayrushinFadis')
+	timeVariable.style.color = '#666666'
+	timeVariable.style.textShadow = 'Black 0 0 10px'
+	console.log(timeVariable)
+	
+	// body
+	timeVariable = document.getElementById('body')
+	timeVariable.style.backgroundColor = '#292929'
+	console.log(timeVariable)
+	
+	// div 1
+	timeVariable = document.getElementById('div1')
+	timeVariable.style.backgroundColor = '#292929'
+	console.log(timeVariable)
+	
+	// div 2
+	timeVariable = document.getElementById('div2')
+	timeVariable.style.backgroundColor = '#292929'
+	console.log(timeVariable)
+		
+	// footer
+	timeVariable = document.getElementById('footer')
+	timeVariable.style.backgroundColor = '#191919'
+	console.log(timeVariable)
 }
 
-function whiteTheme()
+function themeWhite()
 {
-	let timeBody = document.getElementById('themeBody')
-	timeBody.style.backgroundColor = '#3333CC'
-	let timeNav = document.getElementById('themeNav')
-	timeNav.style.backgroundColor = '#3300CC'
-	let timeHeader = document.getElementById('themeHeader')
-	timeHeader.style.backgroundColor = '#3300CC'
-	let timeFooter = document.getElementById('themeFooter')
-	timeFooter.style.backgroundColor = '#3300CC'
+	let timeVariable = "";
+	
+	// Fayrushin Fadis
+	timeVariable = document.getElementById('FayrushinFadis')
+	timeVariable.style.color = 'DarkBlue'
+	timeVariable.style.textShadow = 'steelblue 0 0 10px'
+	console.log(timeVariable)
+	
+	// body
+	timeVariable = document.getElementById('body')
+	timeVariable.style.backgroundColor = 'MidnightBlue'
+	console.log(timeVariable)
+	
+	// div 1
+	timeVariable = document.getElementById('div1')
+	timeVariable.style.backgroundColor = 'MidnightBlue'
+	console.log(timeVariable)
+	
+	// div 2
+	timeVariable = document.getElementById('div2')
+	timeVariable.style.backgroundColor = 'MidnightBlue'
+	console.log(timeVariable)
+		
+	// footer
+	timeVariable = document.getElementById('footer')
+	timeVariable.style.backgroundColor = 'RoyalBlue'
+	console.log(timeVariable)
 }
